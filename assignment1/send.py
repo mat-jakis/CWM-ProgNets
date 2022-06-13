a65969df54a7ba0383f464e732a8abc0572b370d
mat-jakis <98180387+mat-jakis@users.noreply.github.com>
2022-06-13 12:45:21 +0100

    Fix errors

--- assignment1/send.py
+++ assignment1/send.py
@@ -6,7 +6,7 @@ import random, string
 
 
 def randomword(max_length):
-    length = random.randint(22, max_length)
+    length = random.randint(10, max_length)
     return ''.join(random.choice(string.ascii_lowercase) for i in range(int(length)))
 
 def send_random_traffic(num_packets, interface, src_ip, dst_ip):
